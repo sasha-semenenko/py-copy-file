ba5e2d3232f2c5d64ed42949a8b8daa05b0a2d9a
Sasha Semenenko <semenenko233249@gmail.com>
2023-03-14 18:13:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    string, first_command, second_command = command.split(" ")
+    string, first_command, second_command = command.split(",")
     if string == "cp" and first_command != second_command:
         with open("first_command", "r") as first_file, \
                 open("second_command", "w") as second_file:
